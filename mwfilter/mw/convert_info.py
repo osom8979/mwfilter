67973a0d3eb1cbe091b755f403613716e419eaa4
zer0 <osom8979@gmail.com>
2025-02-04 14:42:36 +0900

    Create ConvertInfo

---
+++ mwfilter/mw/convert_info.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+import json
+import urllib.parse
+from dataclasses import dataclass, field
+from io import StringIO
+from pathlib import Path
+
+from pypandoc import convert_file
+from type_serialize import deserialize
+
+from mwfilter.mw.page_meta import PageMeta
+
+
+@dataclass
+class ConvertInfo:
+    meta_path: str = field(default_factory=str)
+    text_path: str = field(default_factory=str)
+    meta: PageMeta = field(default_factory=lambda: PageMeta())
+    text: str = field(default_factory=str)
+
+    @classmethod
+    def from_paths(cls, meta_path: Path, text_path: Path):
+        return cls(
+            meta_path=str(meta_path),
+            text_path=str(text_path),
+            meta=deserialize(json.loads(meta_path.read_bytes()), PageMeta),
+            text=text_path.read_text(),
+        )
+
+    @property
+    def title(self):
+        return self.meta.name
+
+    @property
+    def filename(self):
+        return self.meta.filename
+
+    @property
+    def date(self):
+        return self.meta.touched.date().isoformat()
+
+    @property
+    def url_title(self):
+        return urllib.parse.quote(self.title)
+
+    @property
+    def yaml_frontmatter(self):
+        buffer = StringIO()
+        buffer.write("---\n")
+        buffer.write(f"title: {self.title}\n")
+        buffer.write(f"date: {self.date}\n")
+        buffer.write("---\n")
+        buffer.write("\n")
+        return buffer.getvalue()
+
+    def as_markdown(self) -> str:
+        return self.yaml_frontmatter + convert_file(
+            self.text_path,
+            to="markdown",
+            format="mediawiki",
+        )
