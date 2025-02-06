bfc61e7848c74b3b1c1034626908b0d9f3f66b3c
zer0 <osom8979@gmail.com>
2025-02-06 09:37:14 +0900

    Create PandocAstParser

--- mwfilter/pandoc/mediawiki/converter.py
+++ mwfilter/pandoc/mediawiki/converter.py
@@ -44,23 +44,6 @@ def on_block(block: dict):
         case _:
             raise ValueError(f"Unexpected block type: {block_type}")
 
-"""
-Plain [Inline]	Plain text, not a paragraph
-Para [Inline]	Paragraph
-LineBlock [[Inline]]	Multiple non-breaking lines
-CodeBlock Attr Text	Code block (literal) with attributes
-RawBlock Format Text	Raw block
-BlockQuote [Block]	Block quote (list of blocks)
-OrderedList ListAttributes [[Block]]	Ordered list (attributes and a list of items, each a list of blocks)
-BulletList [[Block]]	Bullet list (list of items, each a list of blocks)
-DefinitionList [([Inline], [[Block]])]	Definition list. Each list item is a pair consisting of a term (a list of inlines) and one or more definitions (each a list of blocks)
-Header Int Attr [Inline]	Header - level (integer) and text (inlines)
-HorizontalRule	Horizontal rule
-Table Attr Caption [ColSpec] TableHead [TableBody] TableFoot	Table, with attributes, caption, optional short caption, column alignments and widths (required), table head, table bodies, and table foot
-Figure Attr Caption [Block]	Figure, with attributes, caption, and content (list of blocks)
-Div Attr [Block]	Generic block container with attributes
-"""
-
 
 def default_pandoc_filter(obj):
     assert isinstance(obj, dict)
